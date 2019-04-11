6a3c2af8d3cb97bf43728b597091255c3311944d
rustg1n <rustg1n@163.com>
2019-04-11 17:06:10 +0800

    forwarding

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import os
-filepath="C:\\Users\\Administrator\\Desktop\\Python"
+import re
+filepath="C:\\Users\\Administrator\\Desktop\\Python\\demolist"
 demolist=os.listdir(filepath)     #储存文件夹目录下所有文件名到demolist中
+print(demolist)
+
+for demo in demolist:
+    mapname=re.findall(r'(.+?)\_shNz',demo)
+    print(mapname)
